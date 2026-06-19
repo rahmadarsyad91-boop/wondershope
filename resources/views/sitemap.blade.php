4af603b26b28c72a016dbe64bed79d567c03acb2
rahmadarsyad91-boop <rahmadarsyad91@gmail.com>
2026-06-19 14:03:58 +0800

    fix: use @php directive in sitemap to fix IDE syntax warning

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,4 +1,4 @@
-{!! '<?xml version="1.0" encoding="UTF-8"?>' !!}
+@php echo '<?xml version="1.0" encoding="UTF-8"?>'; @endphp
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <url><loc>{{ url('/') }}</loc><changefreq>daily</changefreq><priority>1.0</priority><lastmod>{{ now()->toW3cString() }}</lastmod></url>
 <url><loc>{{ url('/login') }}</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>
