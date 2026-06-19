466609dacaf1d217bd43553ea82fc349e40d0332
rahmadarsyad91-boop <rahmadarsyad91@gmail.com>
2026-06-19 14:02:51 +0800

    fix: clean sitemap XML to fix Google Search Console parsing error

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,34 +1,9 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+{!! '<?xml version="1.0" encoding="UTF-8"?>' !!}
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-
-    {{-- Halaman Utama --}}
-    <url>
-        <loc>{{ url('/') }}</loc>
-        <changefreq>daily</changefreq>
-        <priority>1.0</priority>
-        <lastmod>{{ now()->toAtomString() }}</lastmod>
-    </url>
-
-    {{-- Halaman Login & Register --}}
-    <url>
-        <loc>{{ url('/login') }}</loc>
-        <changefreq>monthly</changefreq>
-        <priority>0.5</priority>
-    </url>
-    <url>
-        <loc>{{ url('/register') }}</loc>
-        <changefreq>monthly</changefreq>
-        <priority>0.5</priority>
-    </url>
-
-    {{-- Halaman Detail Produk (Dinamis dari DB) --}}
-    @foreach ($products as $product)
-    <url>
-        <loc>{{ url('/product/' . $product->slug) }}</loc>
-        <changefreq>weekly</changefreq>
-        <priority>0.8</priority>
-        <lastmod>{{ $product->updated_at->toAtomString() }}</lastmod>
-    </url>
-    @endforeach
-
+<url><loc>{{ url('/') }}</loc><changefreq>daily</changefreq><priority>1.0</priority><lastmod>{{ now()->toW3cString() }}</lastmod></url>
+<url><loc>{{ url('/login') }}</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>
+<url><loc>{{ url('/register') }}</loc><changefreq>monthly</changefreq><priority>0.5</priority></url>
+@foreach ($products as $product)
+<url><loc>{{ url('/product/' . $product->slug) }}</loc><changefreq>weekly</changefreq><priority>0.8</priority><lastmod>{{ $product->updated_at->toW3cString() }}</lastmod></url>
+@endforeach
 </urlset>
